946443286f4d603cfff4a0ee9bdd5105f09bc5b5
Thea <142231299+QueenWoofie@users.noreply.github.com>
2025-04-29 12:03:29 +0200

    Login / Logout / Register fix

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import "./css/style.css";
 
+import { navHeader } from "./js/ui/global/Header.js";
+import { setLogoutListener } from "./js/ui/global/logout.js";
+
 import router from "./js/router";
+
+document.querySelector("header").innerHTML = navHeader();
+setLogoutListener();
 
 await router(window.location.pathname);
